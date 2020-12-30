a4016f61b3810dbdfcb2996f9a99b00af6378b28
ted55665566 <ted55665566@gmail.com>
2020-12-30 15:52:10 +0800

    footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>
+		$_REQUEST存取表單資料測試
+	</title>
+</head>
+<body>
+<hr>
+
+
+<div style="text-align:center">
+  <span>
+   <a href='/'>回到我的網站</a>
+  </span>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    版權所有，請尊重智慧財產權
+   
+</div>
+<hr>

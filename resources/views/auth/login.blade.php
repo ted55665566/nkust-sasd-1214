558f7788863fd4eebc0ee89b021f09d83379674c
ted55665566 <ted55665566@gmail.com>
2021-01-06 18:08:00 +0800

    heroku final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,10 @@
+<title>陳志維登入畫面</title>
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
         </x-slot>
+        
 
         <x-jet-validation-errors class="mb-4" />
 

2881d075111f63d437fd6a171fe6ccdbc4a5dfb0
ted55665566 <ted55665566@gmail.com>
2021-01-06 19:55:34 +0800

    heroku final

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -48,7 +48,7 @@
      @auth
      <form method="post" action="/insert">
         @csrf
-     最新消息:<input type=text size=40 name=title>
+     最新消息:<input type=text size=40 name=title required='required'>
      <input type=submit value="新增">
      </form>
      @endauth
@@ -82,5 +82,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     -->
+    @include('layouts.footer')
   </body>
 </html>
